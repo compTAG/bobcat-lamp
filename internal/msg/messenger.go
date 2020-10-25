e261c6244447298db4804a8412f367f0446beb12
David Millman <dave@cs.unc.edu>
2020-10-25 12:32:41 -0600

    Add sms messenger

--- internal/msg/messenger.go
+++ internal/msg/messenger.go
@@ -22,3 +22,26 @@ func (m DummyMessenger) Send(message Message) (string, error) {
 	m.logger.Printf("DummyMessenger: (%s) %s", message.To().FullName(), message.Body())
 	return "", nil
 }
+
+type SmsMessenger struct {
+	client SmsClient
+	from   types.PhoneNumber
+}
+
+func MakeSmsMessenger(client SmsClient, from types.PhoneNumber) SmsMessenger {
+	return SmsMessenger{client, from}
+}
+
+func (m SmsMessenger) Send(message Message) (string, error) {
+	response, err := m.client.Send(
+		m.from,
+		message.To().CellPhoneNumber(),
+		message.Body(),
+	)
+
+	sid := ""
+	if err == nil {
+		sid = response.Sid
+	}
+	return sid, err
+}
--- internal/msg/messenger_test.go
+++ internal/msg/messenger_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/comptag/bobcat-lamp/internal/msg"
 	"github.com/comptag/bobcat-lamp/internal/types"
+	"github.com/comptag/bobcat-lamp/mocks"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,4 +23,43 @@ func TestDummyMessenger(t *testing.T) {
 	r, err := messenger.Send(message)
 	assert.Equal(t, "", r)
 	assert.NoError(t, err)
+}
+
+func TestSmsMessenger(t *testing.T) {
+
+	cell := types.MakePhoneNumber("1112223333")
+	to := types.MakePatient("", "", cell)
+	body := "a boring message"
+	message := msg.MakeMessage(to, body)
+
+	serverPhone := types.MakePhoneNumber("4445556666")
+
+	t.Run("send success", func(t *testing.T) {
+		sid := "a-sid"
+
+		client := new(mocks.SmsClient)
+		response := msg.SmsResponse{Sid: sid}
+		client.On("Send", serverPhone, cell, body).Return(&response, nil)
+
+		messenger := msg.MakeSmsMessenger(client, serverPhone)
+
+		r, err := messenger.Send(message)
+		client.AssertExpectations(t)
+		assert.Equal(t, sid, r)
+		assert.NoError(t, err)
+	})
+
+	t.Run("send fails", func(t *testing.T) {
+
+		client := new(mocks.SmsClient)
+		err := errors.New("an error")
+		client.On("Send", serverPhone, cell, body).Return(nil, err)
+
+		messenger := msg.MakeSmsMessenger(client, serverPhone)
+
+		r, err := messenger.Send(message)
+		client.AssertExpectations(t)
+		assert.Equal(t, "", r)
+		assert.Error(t, err)
+	})
 }
