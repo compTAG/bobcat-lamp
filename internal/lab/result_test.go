40a1af7e58fba9a5b76747c80b8851edc1a0b3b0
David Millman <dave@cs.unc.edu>
2020-10-24 18:59:07 -0600

    Add lab result interface

--- internal/lab/result_test.go
+++ internal/lab/result_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/comptag/bobcat-lamp/internal/types"
 )
 
+func TestResultInterface(t *testing.T) {
+	// if result is not a lab result this code will cause compilation to fail
+	phone := types.MakePhoneNumber("123")
+	var _ types.LabResult = lab.MakeResult("", "", phone, false)
+}
+
 func TestResultGetter(t *testing.T) {
 	phone := types.MakePhoneNumber("123")
 	result := lab.MakeResult("abc", "bob c lamptest", phone, false)
--- internal/types/types.go
+++ internal/types/types.go
@@ -5,3 +5,10 @@ type PhoneNumber string
 func MakePhoneNumber(p string) PhoneNumber {
 	return PhoneNumber(p)
 }
+
+type LabResult interface {
+	Id() string
+	FullName() string
+	CellPhoneNumber() PhoneNumber
+	IsPositive() bool
+}
