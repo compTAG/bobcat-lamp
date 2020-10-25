4f9f6ac3b225aa9fa3a7c1e25e7bb482c0f5dbfc
David Millman <dave@cs.unc.edu>
2020-10-25 12:45:46 -0600

    Add basic sms client

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/comptag/bobcat-lamp
 
 go 1.14
 
-require github.com/stretchr/testify v1.6.1
+require (
+	github.com/sfreiberg/gotwilio v0.0.0-20200916182813-169c4cd5c691
+	github.com/stretchr/testify v1.6.1
+)
---
+++ internal/msg/smsclient.go
@@ -0,0 +1,45 @@
+package msg
+
+import (
+	"errors"
+
+	"github.com/comptag/bobcat-lamp/internal/types"
+	"github.com/sfreiberg/gotwilio"
+)
+
+type SmsResponse = gotwilio.SmsResponse
+
+type SmsClient interface {
+	Send(from, to types.PhoneNumber, body string) (*SmsResponse, error)
+	// Get(sid string) (*SmsResponse, error)
+}
+
+type TwilioClient struct {
+	twilio *gotwilio.Twilio
+}
+
+func MakeTwilioClient(accountSid, authToken string) TwilioClient {
+	twilio := gotwilio.NewTwilioClient(accountSid, authToken)
+	return TwilioClient{twilio}
+}
+
+func mergeError(excep *gotwilio.Exception, err error) error {
+	var result error = nil
+	if err != nil {
+		result = err
+	} else if excep != nil {
+		result = errors.New(excep.Error())
+	}
+	return result
+}
+
+func (c TwilioClient) Send(from, to types.PhoneNumber, body string) (*SmsResponse, error) {
+	response, excep, err := c.twilio.SendSMS(
+		from.InternationalNoDash(),
+		to.InternationalNoDash(),
+		body,
+		"",
+		"",
+	)
+	return response, mergeError(excep, err)
+}
